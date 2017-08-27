bc2cc86ce41ebaa1233635f95a8fdb3cb37d7e8a
JarrodCColburn <jcourtland@icloud.com>
2017-08-26 21:27:26 -0500

    feat: Events Class

---
+++ src/components/event.js
@@ -0,0 +1,12 @@
+class event {
+  constructor(host, title, location, date, description, category, genre) {
+    this.host = host;
+    this.title = title;
+    this.location = location;
+    this.date = date;
+    this.description = description;
+    this.category = category;
+    this.genre = genre;
+  }
+}
+default export event
